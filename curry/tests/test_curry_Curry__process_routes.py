c138e639f6303f33607419a547af21b3f05a81ed
Bryan Davidson <bryan.davidson@rackspace.com>
2013-05-03 16:58:27 -0400

    Configure routes
    * Routes loaded from /etc/curry/routes.json or from a specified path
    * Pass-through routes configurable
    * Multi-endpoint routes configuraable
    * All surrounded with 100% test code coverage

---
+++ curry/tests/test_curry_Curry__process_routes.py
@@ -0,0 +1,24 @@
+from testtools import ExpectedException, TestCase
+
+from curry.curry import Curry
+from curry.errors import RouteError
+
+
+class TestCurryCurry_Process_Routes(TestCase):
+    def test_config_invalid_json(self):
+        with ExpectedException(RouteError):
+            Curry('curry/tests/etc/routes.invalid_json.json')
+
+    def test_config_invalid_path(self):
+        with ExpectedException(IOError):
+            Curry('curry/tests/etc/missing_file.json')
+
+    def test_config_valid(self):
+        curry = Curry('curry/tests/etc/routes.forwarding_address.json')
+
+        self.assertEqual(1, len(curry._routes))
+
+    def test_config_multiple_routes(self):
+        curry = Curry('curry/tests/etc/routes.forwarding_addresses.json')
+
+        self.assertEqual(2, len(curry._routes))
