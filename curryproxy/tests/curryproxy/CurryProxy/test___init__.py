fa89aa1e8fbd495e20928b2e112e65a635a07d3c
Bryan Davidson <bryan.davidson@rackspace.com>
2013-05-23 16:47:34 -0400

    Removed test dependency on /etc/curryproxy/routes.json
    The test now verifies an attempt to open /etc/curryproxy/routes.json instead
    of requiring the file to be present.

--- curryproxy/tests/curryproxy/CurryProxy/test___init__.py
+++ curryproxy/tests/curryproxy/CurryProxy/test___init__.py
@@ -1,3 +1,4 @@
+from mock import Mock
 import testtools
 
 from curryproxy.curryproxy import CurryProxy
@@ -5,13 +6,18 @@ from curryproxy.curryproxy import CurryProxy
 
 class Test___Init__(testtools.TestCase):
     def test_config_default(self):
-        try:
-            CurryProxy()
-        except IOError:
-            self.fail('Requires the default /etc/curry/routes.json file to '
-                      'be present & valid.')
+        old_method = CurryProxy._process_routes
+        CurryProxy._process_routes = Mock()
+
+        CurryProxy()
+
+        default_path = '/etc/curryproxy/routes.json'
+        CurryProxy._process_routes.assert_called_with(default_path)
+
+        CurryProxy._process_routes = old_method
 
     def test_config_supplied(self):
-        curry = CurryProxy('curryproxy/tests/etc/routes.forwarding_address.json')
+        route_file_path = 'curryproxy/tests/etc/routes.forwarding_address.json'
+        curry = CurryProxy(route_file_path)
 
         self.assertEqual(1, len(curry._routes))
