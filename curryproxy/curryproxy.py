b0224c2589dcb0e54e27424b47bd18b2d6d629c1
Bryan Davidson <bryan.davidson@rackspace.com>
2013-06-05 10:01:29 -0400

    Logging
    Any errors encountered while fulfilling a request will now be logged based on
    the specified logging configuration file.

--- curryproxy/curryproxy.py
+++ curryproxy/curryproxy.py
@@ -1,15 +1,31 @@
 import json
+import logging
 
-from webob import Response, Request
+from webob import Request
+from webob import Response
 
+from errors import ConfigError
+from errors import RequestError
 from routes import route_factory
-from errors import ConfigError, RequestError
+
+
+def exception_wrapper(function):
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except Exception as e:
+            logging.exception(e)
+
+    return wrapper
 
 
 class CurryProxy(object):
-    def __init__(self, routes_file='/etc/curryproxy/routes.json'):
+    def __init__(self,
+                 routes_file='/etc/curryproxy/routes.json',
+                 logging_config='/etc/curryproxy/logging.conf'):
         self._routes = []
         self._process_routes(routes_file)
+        logging.config.fileConfig(logging_config)
 
     def _process_routes(self, routes_file):
         try:
@@ -20,6 +36,7 @@ class CurryProxy(object):
         for route_config in route_configs:
             self._routes.append(route_factory.parse_dict(route_config))
 
+    @exception_wrapper
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = None
