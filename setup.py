ead657198b4a13f189a0c8e2c28c316a6d6f6aed
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-07-12 00:00:56 -0400

    fix(setup): point URL to repo, parse reqs better.
    Once open-sourced, one.wiki will not be accessible to the outside
    world. By default, URL points at the github repo as the authoritative
    source of information.
    
    When parsing requirements in setup.py, os.path.join is required to
    handle clean installation on Windows, since '/' isn't recognized as a
    path separator.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+import os
 import setuptools
 
+requires = os.path.join('tools', 'pip-requires')
 
 setuptools.setup(
     name='curryproxy',
@@ -8,10 +10,10 @@ setuptools.setup(
     license="TBD",
     author='Bryan Davidson',
     author_email='bryan.davidson@rackspace.com',
-    url='https://one.rackspace.com/display/atlanta/Curry',
+    url='https://github.com/rackerlabs/Curry',
     packages=setuptools.find_packages(
         exclude=['bin', "*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     test_suite='nose.collector',
-    install_requires=open('tools/pip-requires', 'r').read()
+    install_requires=open(requires).read().splitlines()
 )
