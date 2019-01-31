4cc261f928624967b63ade9d0181ee9ea1bb0149
Andrew Vant <ajvant@gmail.com>
2019-01-31 01:21:08 -0500

    Add wsgi module for use by gunicorn
    This is based off the example given in the wiki. It relieves the user of
    having to supply their own, provided they want to use CP in its default
    configuration.

---
+++ curryproxy/tests/curryproxy/test_wsgi.py
@@ -0,0 +1,29 @@
+from functools import partial
+
+from testtools import TestCase
+from mock import patch
+
+from curryproxy import CurryProxy
+
+
+class TestWsgi(TestCase):
+    def setUp(self):
+        super(TestWsgi, self).setUp()
+        # curryproxy.wsgi uses the default conf file locations, which
+        # are under /etc/curryproxy. That's not appropriate for tests,
+        # so let's override them before importing.
+        patched_cproxy = partial(
+                CurryProxy,
+                routes_file="curryproxy/tests/etc/routes.empty.json",
+                logging_config="curryproxy/tests/etc/logging.console.conf"
+                )
+        # Note that python caches curryproxy.wsgi here, so you can't get
+        # the unpatched version of curryproxy.wsgi.app after this
+        # without reload()ing it. That caused me some puzzlement.
+        with patch("curryproxy.CurryProxy", patched_cproxy):
+            import curryproxy.wsgi
+            self.app = curryproxy.wsgi.app
+
+    def test_wsgiapp(self):
+        self.assertIsInstance(self.app, CurryProxy)
+        self.assertEqual(0, len(self.app._routes))
